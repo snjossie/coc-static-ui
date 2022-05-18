ec9e5d2d9be180815f445e3841fbb085ce73b2ff
snjossie <snjossie@users.noreply.github.com>
2022-05-17 20:06:23 -0500

    Add default case

--- src/AuthConfig.js
+++ src/AuthConfig.js
@@ -39,6 +39,8 @@ export const msalConfig = {
                     case LogLevel.Warning:		
                         console.warn(message);		
                         return;		
+                    default:
+                        return;
                 }	
             }	
         }	
